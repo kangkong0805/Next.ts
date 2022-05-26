1cd3c3370f199d2ff31577d7266293828bf339c5
kangkong <kang3530554@gmail.com>
2022-05-26 23:59:34 +0900

    1.3 Routing까지 강의 학습

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+import NavBar from '../components/NavBar'
 
 const Home: NextPage = () => {
   return (
-    <>
-    </>
+    <div>
+      <NavBar />
+      <h1>Hello</h1>
+    </div>
   )
 }
 
