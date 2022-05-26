ced9fe7972015daee7326d8fc345e18ec6a8f091
kangkong <kang3530554@gmail.com>
2022-05-27 08:07:41 +0900

    1.5 styles JSX 학습

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBAr.module.css"; // css 모듈
 
 export default function NavBar() {
     const router = useRouter();
     return (
-        /* className={JS object의 properties 형식}*/
         <nav>
-            <Link href="/"><a className={router.pathname === '/' ? styles.active : ""}>Home</a></Link>
-            <Link href="/about"><a className={router.pathname === '/about' ? styles.active : ""}>About</a></Link>
+            <Link href="/"><a className={router.pathname === "/" ? "active" : ""}>Home</a></Link>
+            <Link href="/about"><a className={router.pathname === "/about" ? "active" : ""}>About</a></Link>
+            <style jsx>{`
+                nav{
+                    background: tomato;
+                }
+                a{
+                    text-decoration: none;
+                }
+                .active{
+                    color: yellow;
+                }
+            `}</style>
         </nav>
     )
 }
