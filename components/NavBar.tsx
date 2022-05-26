0c948ff91b14cddf4f129bcc6a9d7523759fef9d
kangkong <kang3530554@gmail.com>
2022-05-27 07:53:52 +0900

    1.4 CSS Modules 학습

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBAr.module.css"; // css 모듈
 
-export default function NavBar(){
-    return(
+export default function NavBar() {
+    const router = useRouter();
+    return (
+        /* className={JS object의 properties 형식}*/
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">about</Link>   
+            <Link href="/"><a className={router.pathname === '/' ? styles.active : ""}>Home</a></Link>
+            <Link href="/about"><a className={router.pathname === '/about' ? styles.active : ""}>About</a></Link>
         </nav>
     )
 }
